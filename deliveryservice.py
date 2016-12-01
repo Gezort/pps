ad096ddbfa393cdeb99bc44f9c5230521f022b45
Gezort <miron_55@mail.ru>
2016-12-01 19:58:11 +0300

    add delivery service class

---
+++ deliveryservice.py
@@ -0,0 +1,37 @@
+class DeliveryService():
+
+    def __init__(self, graph, history, orders, launchQueue, currentOrders, operator):
+        pass
+
+    def launchOrder(self, id):
+        pass
+
+    def addOrder(self):
+        pass
+
+    def deleteOrder(self, id):
+        pass
+
+    def getOrderInfo(self):
+        pass
+
+    def addItemToOrder(self):
+        pass
+
+    def deleteItemFromOrder(self):
+        pass
+
+    def buildRouteForOrder(self):
+        pass
+
+    def getLocation(self):
+        pass
+
+    def moveOrder(self):
+        pass
+
+    def confirmOrderTransition(self):
+        pass
+
+    def lookupHistory(self):
+        pass
